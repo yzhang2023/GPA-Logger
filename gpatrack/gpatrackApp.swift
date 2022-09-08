a2216cb2fe4165049e72972112eb0daff602fe21
catdsnny <shaun@shaunneal.com>
2022-09-07 19:32:30 -0700

    Initial Commit

---
+++ gpatrack/gpatrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpatrackApp.swift
+//  gpatrack
+//
+//  Created by David Neal on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct gpatrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
